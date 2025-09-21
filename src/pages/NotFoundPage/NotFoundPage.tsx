802c789729df455f78affaedde4dd038fac67707
Ajay Yadav <yadav.ajay@ketto.org>
2025-09-21 15:39:33 +0530

    feat: test 2

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const NotFoundPage: React.FC = () => {
-  return <div>dev NotFoundPage</div>;
+  return <div>dev1 NotFoundPage</div>;
 };
 
 export default NotFoundPage;
